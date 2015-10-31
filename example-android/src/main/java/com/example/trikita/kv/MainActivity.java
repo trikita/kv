330407a3f34efd89f58a248aea424eb2dfe056d7
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2015-10-31 14:24:40 +0200

    added benchmark for various storage/encoding types

--- example-android/src/main/java/com/example/trikita/kv/MainActivity.java
+++ example-android/src/main/java/com/example/trikita/kv/MainActivity.java
@@ -1,52 +1,159 @@
 package com.example.trikita.kv;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.File;
+import java.io.Serializable;
+import java.util.ArrayList;
 
 import trikita.kv.FileStorage;
-import trikita.kv.SharedPrefsStorage;
 import trikita.kv.KV;
+import trikita.kv.LruStorage;
+import trikita.kv.GsonEncoder;
 import trikita.kv.SerializedEncoder;
+import trikita.kv.SharedPrefsStorage;
+import trikita.kv.SqliteStorage;
 
 public class MainActivity extends Activity {
 
-	static {
-		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-				.detectDiskReads()
-				.detectDiskWrites()
-				.detectAll()
-				.penaltyLog()
-				.build());
-		StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-				.detectLeakedSqlLiteObjects()
-				.detectLeakedClosableObjects()
-				.penaltyLog()
-				.penaltyDeath()
-				.build());
-	}
-
-	private KV fileDB =
-		new KV(new FileStorage(new File("/data/data/com.example.trikita.kv/kv.db")),
-				new SerializedEncoder());
-	private KV prefDB;
+	public final static int ITERATIONS = 100;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		prefDB = new KV(new SharedPrefsStorage(this, "kv"), new SerializedEncoder());
+		final Button button = (Button) findViewById(R.id.start_button);
+		final TextView textView = (TextView) findViewById(R.id.results);
 
-		fileDB.set("foo", "bar");
-		prefDB.set("foo", "baz");
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				final StringBuilder sb = new StringBuilder();
+				button.setEnabled(false);
+				new AsyncTask<Void, Void, Void>() {
+					public Void doInBackground(Void... args) {
+						KV kv;
 
-		String bar = fileDB.get("foo");
-		System.out.println("Bar = " + bar);
+						kv = new KV(
+							new SharedPrefsStorage(MainActivity.this, "kv"),
+							new SerializedEncoder());
+						benchKV("SharedPrefs/Serialized", kv, sb);
+						publishProgress();
 
-		String baz = prefDB.get("foo");
-		System.out.println("Baz = " + baz);
+						kv = new KV(
+							new SharedPrefsStorage(MainActivity.this, "kv"),
+							new GsonEncoder());
+						benchKV("SharedPrefs/Gson", kv, sb);
+						publishProgress();
+
+						kv = new KV(
+							new SqliteStorage(MainActivity.this, "kv"),
+							new SerializedEncoder());
+						benchKV("Sqlite/Serialized", kv, sb);
+						publishProgress();
+
+						kv = new KV(
+							new SqliteStorage(MainActivity.this, "kv"),
+							new GsonEncoder());
+						benchKV("Sqlite/Gson", kv, sb);
+						publishProgress();
+
+						kv = new KV(
+							new LruStorage(new SqliteStorage(MainActivity.this, "kv"),
+								ITERATIONS),
+							new SerializedEncoder());
+						benchKV("Sqlite+LRU/Serialized", kv, sb);
+						publishProgress();
+
+						kv = new KV(
+							new LruStorage(new SqliteStorage(MainActivity.this, "kv"),
+								ITERATIONS),
+							new GsonEncoder());
+						benchKV("Sqlite+LRU/Gson", kv, sb);
+						publishProgress();
+						return null;
+					}
+
+					public void onProgressUpdate(Void... values) {
+						textView.setText(sb.toString());
+					}
+					public void onPostExecute(Void result) {
+						button.setEnabled(true);
+					}
+				}.execute();
+			}
+		});
+	}
+
+	private void benchKV(String name, final KV kv, StringBuilder out) {
+		out.append("=== TEST: " + name + "\n");
+		out.append(bench("SET primitives", new Runnable() {
+			public void run() {
+				kv.set("foo", "bar");
+				kv.set("bar", true);
+				kv.set("baz", 123);
+			}
+		}, 3));
+
+		out.append(bench("GET primitives", new Runnable() {
+			public void run() {
+				String s = kv.get("foo");
+				boolean b = kv.get("bar");
+				int n = kv.get("baz");
+			}
+		}, 3));
+
+		out.append(bench("SET object", new Runnable() {
+			public void run() {
+				kv.set("user", new User("John", "Doe"));
+			}
+		}, 1));
+
+		out.append(bench("GET object", new Runnable() {
+			public void run() {
+				User u = kv.get("user");
+			}
+		}, 1));
+
+		final ArrayList<User> users = new ArrayList<>();
+		for (int i = 0; i < 100; i++) {
+			users.add(new User("John", "Doe"+i));
+		}
+		out.append(bench("SET list", new Runnable() {
+			public void run() {
+				kv.set("users", users);
+			}
+		}, 1));
+
+		out.append(bench("GET list", new Runnable() {
+			public void run() {
+				ArrayList<User> u = kv.get("users");
+			}
+		}, 1));
+		kv.close();
+	}
+
+	private String bench(String name, Runnable r, int ops) {
+		long start = System.nanoTime();
+		for (int i = 0; i < ITERATIONS; i++) {
+			r.run();
+		}
+		long speed = (System.nanoTime() - start) / ITERATIONS;
+		return name + ":" + speed/1000/ops + " us/op\n";
+	}
+
+	public static class User implements Serializable {
+		public final String firstName;
+		public final String lastName;
+		public User(String firstName, String lastName) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+		}
 	}
 }
